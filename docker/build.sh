dd3e79d6fda1b3e3511dea4d3195779d606e7ba3
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2020-11-16 00:00:38 +0300

    Docker: do not update too often

--- docker/build.sh
+++ docker/build.sh
@@ -4,9 +4,8 @@ docker build --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)"  -t
 docker run -ti -v $PWD/..:/sandbox:rw -w /sandbox oe-builder bash -lc "${1:-\
 set -xueo pipefail; \
 pwd; \
-echo ./oebb.sh config; \
-source build-environment-trik; \
-echo bitbake -c cleanall trik-image-core trik-runtime-qt5; \
+[ -r build-environment-trik ] && source build-environment-trik || ./oebb.sh config ; \
+bitbake -c cleanall trik-image-core trik-runtime-qt5; \
 time bitbake trik-image-core -k \
 }"
 #sudo chown -cR --from root:root $USER:$USER .
