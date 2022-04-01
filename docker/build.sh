020c47da14d45bbe5a345fed296b370c3d058e37
Kirill Smirnov <kirill.k.smirnov@gmail.com>
2022-04-01 17:10:17 +0300

    docker: fix startup logic
    The env file is not sourced if it is just created. Fix it.

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ docker build --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)"  -t
 docker run -ti -v $PWD/..:/sandbox:rw -w /sandbox oe-builder bash -lc "\
 set -xueo pipefail; \
 pwd; \
-[ -r build-environment-trik ] && source build-environment-trik || ./oebb.sh config ; \
+[ ! -r build-environment-trik ] && ./oebb.sh config; source build-environment-trik; \
 time eval ${1:-'bitbake -c cleanall trik-image-core trik-runtime-qt5;time bitbake trik-image-core -k'} \
 "
 #sudo chown -cR --from root:root $USER:$USER .
