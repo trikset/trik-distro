f7c4991d3c02a0934fe2ab8762c6729217c0802d
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2024-08-29 16:55:04 +0300

    Pull new kirkstone meta-trik

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 set -xueo pipefail
 pushd $(dirname $(realpath "$BASH_SOURCE"))
 docker build --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)"  -t oe-builder . 
-docker run -ti -v $PWD/..:/sandbox:rw -w /sandbox oe-builder bash -lc "\
+docker run --network=host -ti -v $PWD/..:/sandbox:rw -w /sandbox oe-builder bash -lc "\
 set -xeo pipefail; \
 pwd; \
 if [ ! -d 'poky' ]; then git clone --depth 1 -b kirkstone git://git.yoctoproject.org/poky; fi && cd poky && source oe-init-build-env ..; \
