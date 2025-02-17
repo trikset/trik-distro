b4acc4f8dfa9acd6909333510b34559f2afe1cb6
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2025-02-17 22:12:12 +0300

    Improve usage experience
    and update submodules
    
    * add Poky as submodule
    * initialize dummy `git` repo in `/downloads` to save from `git clean -fdx`
    * add Dependabot for git submodules
    * improve `.gitignore`

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xueo pipefail
 pushd $(dirname $(realpath "$BASH_SOURCE"))
-docker build --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)"  -t oe-builder . 
-docker run --network=host -ti -v $PWD/..:/sandbox:rw -w /sandbox oe-builder bash -lc "\
+docker build --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)"  -t oe-builder-yocto .
+docker run --network=host -ti -v $PWD/..:/sandbox:rw -w /sandbox oe-builder-yocto bash -lc "\
 set -xeo pipefail; \
 pwd; \
-if [ ! -d 'poky' ]; then git clone --depth 1 -b kirkstone git://git.yoctoproject.org/poky; fi && cd poky && source oe-init-build-env ..; \
+cd poky && source oe-init-build-env ..; git -C downloads status 2>&1 1>/dev/null  || git -C downloads -c init.defaultBranch=dummy init ; \
 time eval ${1:-'bitbake -c cleanall trik-image-core trik-runtime-qt5;time bitbake trik-image-core -k'} \
 "
 popd
