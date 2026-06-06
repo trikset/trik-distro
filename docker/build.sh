0341c62be2194a8403e863b3b0c412b4f1165abc
Iakov 'Jake' Kirilenko <jake.kirilenko@gmail.com>
2026-06-06 15:57:11 +0300

    Make build wrapper script Podman-compatible

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+#Podman-compatible docker-based wrapper
 set -xueo pipefail
 pushd $(dirname $(realpath "$BASH_SOURCE"))
 docker build --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)"  -t oe-builder-yocto .
-docker run --network=host -ti -v $PWD/..:/sandbox:rw -w /sandbox oe-builder-yocto bash -lc "\
+env PODMAN_USERNS=keep-id docker run --init --network=private --sig-proxy --rm -ti --pids-limit=-1 --shm-size=3g \
+        --env PRSERV_HOST=localhost:0 --env CACHE=/sandbox-cache --env SSTATE_DIR=/sandbox-sstate \
+        --env DL_DIR=/sandbox/cache \
+        --env SSTATE_DIR=/sandbox/sstate-cache \
+        --env BB_ENV_PASSTHROUGH_ALLOWED="DL_DIR SSTATE_DIR PRSERV_HOST CACHE" \
+	-v $(realpath $PWD/..):/sandbox:rw \
+	-v $(realpath $PWD/..)/cache:/sandbox-cache:rw \
+	-v $(realpath $PWD/..)/sstate-cache:/sandbox-sstate:rw \
+	-w /sandbox oe-builder-yocto bash -lc "\
 set -xeo pipefail; \
 pwd; \
 cd poky && source oe-init-build-env ..; git -C downloads status 2>&1 1>/dev/null  || git -C downloads -c init.defaultBranch=dummy init ; \
-time eval ${1:-'bitbake -c cleanall trik-image-core trik-runtime-qt5;time bitbake trik-image-core -k'} \
+exec bash -c \"time eval ${1:-'bitbake -c cleanall trik-image-core trik-runtime-qt5;time bitbake trik-image-core -k'}\" \
 "
 popd
